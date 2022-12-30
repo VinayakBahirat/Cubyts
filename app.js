12182750417f2a2ced5605910f2e28c277c2000c
vinayak <vinayak.geexu@gmail.com>
2022-12-30 12:49:44 +0530

    first commit

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ const menu = document.querySelector ("#menu")
 
 hamburger.addEventListener("click", ()=> {
     menu.classList.toggle("hidden")
-    hamburger.classList.toggle("bg-white")
+    hamburger.classList.toggle("text-white")
 })
